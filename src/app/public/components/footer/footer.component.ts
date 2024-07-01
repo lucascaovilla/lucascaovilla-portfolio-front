fc473a1d3ef881369b3b9e0abf07b1fd6ae3cc19
lucas caovilla <lucasgrisac@gmail.com>
2024-07-01 15:01:34 -0300

    Updating application footer.

--- src/app/public/components/footer/footer.component.ts
+++ src/app/public/components/footer/footer.component.ts
@@ -8,5 +8,63 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  contactsList: any[] = [
+    {
+      "link": "mailto:lucasgrisac@gmail.com",
+      "target": "",
+      "rel": "",
+      "iconSrc": "../../../../assets/icons/socials/envelope.svg",
+      "iconAlt": "email",
+    },
+    {
+      "link": "https://dev.to",
+      "target": "_blank",
+      "rel": "noopener noreferrer",
+      "iconSrc": "../../../../assets/icons/socials/devdotto.svg",
+      "iconAlt": "Dev.to",
+    },
+    {
+      "link": "https://github.com/lucascaovilla",
+      "target": "_blank",
+      "rel": "noopener noreferrer",
+      "iconSrc": "../../../../assets/icons/socials/github.svg",
+      "iconAlt": "GitHub",
+    },
+    {
+      "link": "https://www.linkedin.com/in/lucasgcaovilla",
+      "target": "_blank",
+      "rel": "noopener noreferrer",
+      "iconSrc": "../../../../assets/icons/socials/linkedin.svg",
+      "iconAlt": "LinkedIn",
+    },
+    {
+      "link": "https://medium.com/@lucascaovilla",
+      "target": "_blank",
+      "rel": "noopener noreferrer",
+      "iconSrc": "../../../../assets/icons/socials/medium.svg",
+      "iconAlt": "Medium",
+    },
+    {
+      "link": "https://www.youtube.com/@lucascaovilla",
+      "target": "_blank",
+      "rel": "noopener noreferrer",
+      "iconSrc": "../../../../assets/icons/socials/youtube.svg",
+      "iconAlt": "YouTube",
+    }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ]
 }
