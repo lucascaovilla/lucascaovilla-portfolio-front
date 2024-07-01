c150d8d46191f2401de19ccd09e8e8392099562b
lucas caovilla <lucasgrisac@gmail.com>
2024-07-01 15:07:04 -0300

    Creating public home-about-component. Importing home-about-component into public home module home-component.

--- src/app/public/modules/home/home.component.ts
+++ src/app/public/modules/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HomeBannerComponent } from "../../components/home-banner/home-banner.component";
+import { HomeAboutComponent } from "../../components/home-about/home-about.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [HomeBannerComponent]
+    imports: [HomeBannerComponent, HomeAboutComponent]
 })
 export class HomeComponent {
 
